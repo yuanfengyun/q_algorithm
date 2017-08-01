33a0c0fe67bb97d3b0eb7600d40a79fc1908fce1
yungege <273461474@qq.com>
2017-08-01 16:25:06 +0800

    测试代码修改C#版

--- mjlib_c#/test_hu/Program.cs
+++ mjlib_c#/test_hu/Program.cs
@@ -36,111 +36,11 @@ namespace test_hu
             }
             System.Console.WriteLine("");
         }
-        static HashSet<int> tested = new HashSet<int>();
-        static void check_hu(int[] cards, int max_gui_index)
-        {
-           
-
-            for (int i = 0; i < max_gui_index; ++i)
-            {
-                if (cards[i] > 4) return;
-            }
-
-            int num = 0;
-            for (int i = 0; i < 9; ++i)
-            {
-                num = num * 10 + cards[i];
-            }
-
-            if (tested.Contains(num))
-            {
-                return;
-            }
-
-            tested.Add(num);
-
-            for (int i = 0; i < max_gui_index; ++i)
-            {
-                if (!HuLib.getInstance().get_hu_info(cards, null, 0, 0))
-                {
-                    System.Console.Write("测试失败 i=%d\n", i);
-                    print_cards(cards);
-                }
-            }
-        }
-
-        static void gen_auto_table_sub(int[] cards, int level)
-        {
-            for (int i = 0; i < 32; ++i)
-            {
-                int index = -1;
-                if (i <= 17)
-                {
-                    cards[i] += 3;
-                }
-                else if (i <= 24)
-                {
-                    index = i - 18;
-                }
-                else
-                {
-                    index = i - 16;
-                }
-
-                if (index >= 0)
-                {
-                    cards[index] += 1;
-                    cards[index + 1] += 1;
-                    cards[index + 2] += 1;
-                }
-
-                if (level == 4)
-                {
-                    check_hu(cards, 18);
-                }
-                else
-                {
-                    gen_auto_table_sub(cards, level + 1);
-                }
-
-                if (i <= 17)
-                {
-                    cards[i] -= 3;
-                }
-                else
-                {
-                    cards[index] -= 1;
-                    cards[index + 1] -= 1;
-                    cards[index + 2] -= 1;
-                }
-            }
-        }
-
-        static void test_two_color()
-        {
-            System.Console.WriteLine("测试两种花色\n");
-            int[] cards = new int[34]{
-                0,1,1,0,0,0,1,1,1,
-                0,0,1,1,1,0,3,0,0,
-                0,0,3,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,
-            };
-
-            for (int i = 0; i < 18; ++i)
-            {
-                cards[i] = 2;
-                System.Console.Write("将 ");
-                System.Console.WriteLine(i);
-                gen_auto_table_sub(cards, 1);
-                cards[i] = 0;
-            }
-        }
-
         static void test_one()
         {
             int[] cards = {
-                0,0,0,0,1,0,0,0,0,
-                1,1,1,2,2,0,2,2,2,
+                0,0,0,0,2,0,0,0,0,
+                1,2,2,1,0,0,2,2,2,
                 0,0,0,0,0,0,0,0,0,
                 0,0,0,0,0,0,0
             };
@@ -156,7 +56,6 @@ namespace test_hu
                 System.Console.Write("测试成功\n");
             }
         }
-
         static void Main()
         {
             System.Console.Write("test hulib begin...\n");
@@ -165,7 +64,6 @@ namespace test_hu
 
             test_one();
 
-            //    test_two_color();
             System.Console.ReadKey();
         }
     }
