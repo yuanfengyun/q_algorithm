5f2dc099b61ac72212ac01e8b0e2737ce221a29c
云哥哥 <273461474@qq.com>
2017-09-05 12:30:30 +0800

    Update split.h

--- mj_split/split.h
+++ mj_split/split.h
@@ -4,16 +4,14 @@ class split
 public:
 	static bool get_hu_info(char* cards, char cur_card, char gui_index);
 private:
-	static bool foreach_eye(char* cards, char gui_num);
+	static bool foreach_eye(char* cards, char gui_num, int eye_color, int* cache);
 
-	static int check_normal(char* cards, int from, int to, int left_gui);
+	static int check_normal(char* cards, int from, int to, int left_gui, int cache_index, int* cache);
 
 	static int next_split(int n, int gui_num);
 
 	static int one(int n, int gui_num);
 	static int two(int n, int gui_num);
-	static int three(int n, int gui_num);
-	static int four(int n, int gui_num);
 
-	static int check_zi(char* cards, int gui_num);
+	static inline int check_zi(char* cards, int gui_num, int cache_index, int* cache);
 };
