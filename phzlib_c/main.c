5706b1a60bbcd4f5d46be6c6edda8599cd0f75fe
云哥哥 <273461474@qq.com>
2017-10-11 20:19:43 +0800

    Create main.c
    跑胡子，回溯算胡息算法

---
+++ phzlib_c/main.c
@@ -0,0 +1,40 @@
+#include "hulib.h"
+#include "stdio.h"
+#include "time.h"
+
+int main()
+{
+	char cards[] = {
+		1,2,2,1,2,1,2,1,0,1,
+		1,2,2,0,0,0,1,2,0,0
+	};
+
+	char cards1[] = {
+		1,1,2,1,2,1,1,0,0,0,
+		1,2,2,1,1,1,1,1,1,1
+	};
+
+	time_t begin = time(NULL);
+	long long sum = 0;
+	int k = 0;
+	int count = 0;
+	for (int i = 0; i < 10000 * 10000; ++i) {
+		if (k) {
+			sum += get_huxi(cards);
+			count = count + 1;
+			k = 0;
+		}
+		else {
+			sum += get_huxi(cards1);
+			count = count + 1;
+			k = 1;
+		}
+		
+	}
+	printf("胡息: %d\n", sum);
+	printf("时间: %d\n", time(NULL) - begin);
+	printf("次数: %d万次\n", count/10000);
+	getchar();
+
+	return 0;
+}
