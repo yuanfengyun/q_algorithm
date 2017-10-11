edb99605026335b259bcc2f9b34512b92a84422c
云哥哥 <273461474@qq.com>
2017-10-11 21:12:40 +0800

    Update main.c

--- phzlib_c/main.c
+++ phzlib_c/main.c
@@ -2,30 +2,46 @@
 #include "stdio.h"
 #include "time.h"
 
+void test() {
+	char cards[] = {
+		1,2,2,1,2,1,2,1,0,1,
+		1,2,2,1,0,0,0,2,0,0
+	};
+	printf("胡息: %d\n", get_huxi(cards));
+}
+
+void test_eye() {
+	char cards_eye[] = {
+		1,1,2,1,2,1,1,0,0,0,
+		1,2,2,1,0,0,2,1,1,1
+	};
+	printf("胡息: %d\n", get_huxi(cards_eye));
+}
+
 int main()
 {
 	char cards[] = {
 		1,2,2,1,2,1,2,1,0,1,
-		1,2,2,0,0,0,1,2,0,0
+		1,2,2,1,0,0,0,2,0,0
 	};
 
-	char cards1[] = {
+	char cards_eye[] = {
 		1,1,2,1,2,1,1,0,0,0,
-		1,2,2,1,1,1,1,1,1,1
+		1,2,2,1,0,0,2,1,1,1
 	};
 
 	time_t begin = time(NULL);
 	long long sum = 0;
 	int k = 0;
 	int count = 0;
-	for (int i = 0; i < 10000 * 10000; ++i) {
+	for (int i = 0; i < 10000*10000; ++i) {
 		if (k) {
 			sum += get_huxi(cards);
 			count = count + 1;
 			k = 0;
 		}
 		else {
-			sum += get_huxi(cards1);
+			sum += get_huxi(cards_eye);
 			count = count + 1;
 			k = 1;
 		}
